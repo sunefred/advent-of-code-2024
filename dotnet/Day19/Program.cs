d7d685cfebce0b9cfdfbfc884179f0947e8c8149
Sune Frederiksen <sune.frederiksen@combitech.com>
2024-12-20 05:58:06 +0100

    sdf

--- dotnet/Day19/Program.cs
+++ dotnet/Day19/Program.cs
@@ -1,57 +1,35 @@
 ﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
 internal class Program
 {
     static void Main()
     {
-        var input = File.ReadAllText("Data/sample.txt");
+        var input = File.ReadAllText("Data/input.txt");
 
-        //Console.WriteLine(Part1(input));
+        Console.WriteLine(Part1(input));
         Console.WriteLine(Part2(input));
     }
 
     static int Part1(string input)
     {
-        int possibleDesigns = 0;
         var patterns = GetPatterns(input);
-        var designs  = GetDesigns(input);
-
-        foreach (var design in designs)
-        {
-            var sw = Stopwatch.StartNew();
-            var hasMatch = HasMatchingPatterns1(design, patterns);
-            if (hasMatch)
-            {
-                possibleDesigns++;
-            }
-        }
-
-        return possibleDesigns;
+        var designs = GetDesigns(input);
+        var designsWithSolutions = designs.Count(d => HasSolution(d, patterns));
+        return designsWithSolutions;
     }
 
-    static int Part2(string input)
+    static long Part2(string input)
     {
-        int possibleDesigns = 0;
         var patterns = GetPatterns(input);
-        var designs  = GetDesigns(input);
-
-        foreach (var design in designs)
-        {
-            var matching = NumberOfDesigns2(design, patterns);
-            possibleDesigns += matching;
-        //     PrintDesignOptions(design, sequences);
-
-        //     if (sequences.Length > 0)
-        //     {
-        //         possibleDesigns += sequences.Length;
-        //     }
-        }
-
-        return possibleDesigns;
+        var designs = GetDesigns(input);
+        var memo = new Dictionary<string, long>();
+        var totalSolutions = designs.Sum(d => CountSolutions(d, patterns, memo));
+        return totalSolutions;
     }
 
-    static bool HasMatchingPatterns1(string design, string[] patterns)
+    static bool HasSolution(string design, string[] patterns)
     {
         var stack = new Stack<string>();
         stack.Push(design);
@@ -77,86 +55,42 @@ internal class Program
         return false;
     }
 
-    static int  NumberOfDesigns2(string design, string[] patterns)
+    static long CountSolutions(string design, string[] patterns, Dictionary<string, long> memo)
     {
-       
-        // Dict for keeping track of number of matches
-        // For a specific design (so far)
-        var matchDict = new Dictionary<string, int>();
-
-        var stack = new Stack<(string remaining, string[] sequence)>();
-        stack.Push((design, []));
-
-        while (stack.Count > 0)
+        if (memo.ContainsKey(design))
         {
-            var (remaining, sequence) = stack.Pop();
+            return memo[design];
+        }
 
-            if (remaining.Length == 0)
+        if (design.Length == 0)
+        {
+            return 1;
+        }
+
+        long count = 0;
+        foreach (var pattern in patterns)
+        {
+            if (design.StartsWith(pattern))
             {
-                for (int i = 0; i <= sequence.Length; i++)
-                {
-                    var key = string.Join("", sequence[..i]);
-                    if (!matchDict.ContainsKey(key))
-                    {
-                        matchDict[key] = 0;
-                    }
-
-                    matchDict[key] = matchDict[key] + 1;
-                }
-
-                continue;
-            }
-
-            if (matchDict.ContainsKey(remaining))
-            {
-                for (int i = 0; i < sequence.Length; i++)
-                {
-                    var key = string.Join("", sequence[..i]);
-                    if (!matchDict.ContainsKey(key))
-                    {
-                        matchDict[key] = 0;
-                    }
-
-                    matchDict[key] = matchDict[key] + 1;
-                }
-            }
-
-            foreach (var pattern in patterns.Reverse())
-            {
-                if (remaining.StartsWith(pattern))
-                {
-                    string newRemaining = remaining[pattern.Length..];
-                    string[] newSequence = [.. sequence, pattern];
-                    stack.Push((newRemaining, newSequence));
-                }
+                string newDesign = design[pattern.Length..];
+                count += CountSolutions(newDesign, patterns, memo);
             }
         }
 
-        return matchDict[design];
+        memo[design] = count;
+        return count;
     }
-
-    static void PrintDesignOptions(string design, string[][] sequences)
-    {
-            Console.WriteLine(design);
-            Console.WriteLine("---------");
-            foreach (var sequence in sequences)
-            {
-                Console.WriteLine(string.Join(",", sequence));
-            }
-            Console.WriteLine("");
-
-    }   
 
     static string[] GetPatterns(string input)
     {
-        var first = input.Split("\n\r")[0];
+        var first = input.Split("\n\n")[0];
         var matches = Regex.Matches(first, @"(\w+)");
         return [.. matches.Select(m => m.Value)];
     }
 
     static string[] GetDesigns(string input)
     {
-        var second = input.Split("\n\r")[1];
+        var second = input.Split("\n\n")[1];
         var matches = Regex.Matches(second, @"(\w+)");
         return [.. matches.Select(m => m.Value)];
     }
