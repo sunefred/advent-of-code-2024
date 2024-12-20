1824a1502cbe39e3d304425bd3a15af90f8b3b9f
Sune Frederiksen <sune.frederiksen@combitech.com>
2024-12-20 22:22:15 +0100

    Add day 19 solution for dotnet

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Day19;
+﻿using System.Text.RegularExpressions;
+
+namespace Day19;
 
 internal class Program
 {
@@ -15,13 +17,86 @@ internal class Program
         Console.WriteLine(Part2(input));
     }
 
-    static string Part1(string input)
+    static int Part1(string input)
     {
-        return input;
+        var patterns = GetPatterns(input);
+        var designs = GetDesigns(input);
+        var designsWithSolutions = designs.Count(d => HasSolution(d, patterns));
+        return designsWithSolutions;
     }
 
-    static string Part2(string input)
+    static long Part2(string input)
     {
-        return input;
+        var patterns = GetPatterns(input);
+        var designs = GetDesigns(input);
+        var memo = new Dictionary<string, long>();
+        var totalSolutions = designs.Sum(d => CountSolutions(d, patterns, memo));
+        return totalSolutions;
+    }
+
+    static bool HasSolution(string design, string[] patterns)
+    {
+        var stack = new Stack<string>();
+        stack.Push(design);
+
+        while (stack.Count > 0)
+        {
+            var remaining = stack.Pop();
+            if (remaining.Length == 0)
+            {
+                return true;
+            }
+
+            foreach (var pattern in patterns)
+            {
+                if (remaining.StartsWith(pattern))
+                {
+                    string newRemaining = remaining[pattern.Length..];
+                    stack.Push(newRemaining);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    static long CountSolutions(string design, string[] patterns, Dictionary<string, long> memo)
+    {
+        if (memo.ContainsKey(design))
+        {
+            return memo[design];
+        }
+
+        if (design.Length == 0)
+        {
+            return 1;
+        }
+
+        long count = 0;
+        foreach (var pattern in patterns)
+        {
+            if (design.StartsWith(pattern))
+            {
+                string newDesign = design[pattern.Length..];
+                count += CountSolutions(newDesign, patterns, memo);
+            }
+        }
+
+        memo[design] = count;
+        return count;
+    }
+
+    static string[] GetPatterns(string input)
+    {
+        var first = input.Split($"{Environment.NewLine}{Environment.NewLine}")[0];
+        var matches = Regex.Matches(first, @"(\w+)");
+        return [.. matches.Select(m => m.Value)];
+    }
+
+    static string[] GetDesigns(string input)
+    {
+        var second = input.Split($"{Environment.NewLine}{Environment.NewLine}")[1];
+        var matches = Regex.Matches(second, @"(\w+)");
+        return [.. matches.Select(m => m.Value)];
     }
 }
