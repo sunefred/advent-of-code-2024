4508a186c1b2bc30076c369659940399668c7fd4
Sune Frederiksen <sune.frederiksen@combitech.com>
2024-12-20 21:14:33 +0100

    Add day 2 solution for dotnet

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -9,19 +9,43 @@ internal class Program
          * 
          */
 
-        var input = File.ReadAllText("Data/sample.txt");
+        var lines = File.ReadAllLines("Data/sample.txt");
 
-        Console.WriteLine(Part1(input));
-        Console.WriteLine(Part2(input));
+        var levels = lines
+            .Select(line => line.Split())
+            .Select(numbers => numbers.Select(x => int.Parse(x)).ToArray())
+            .ToList();
+
+        Console.WriteLine(Part1(levels));
+        Console.WriteLine(Part2(levels));
     }
 
-    static string Part1(string input)
+    public static int Part1(IEnumerable<int[]> levels)
     {
-        return input;
+        var result = levels
+            .Where(IsSafe)
+            .Count();
+
+        return result;
     }
 
-    static string Part2(string input)
+    public static int Part2(IEnumerable<int[]> levels)
     {
-        return input;
+        var result = levels
+            .Select(level => level.Select<int, int[]>((_, i) => [.. level[..i], .. level[(i + 1)..]]))
+            .Where(newLevels => newLevels.Any(IsSafe))
+            .Count();
+
+        return result;
+    }
+
+    private static bool IsSafe(int[] level)
+    {
+        var diffs = level.Zip(level.Skip(1), (a, b) => b - a).ToArray();
+
+        bool isIncreasing = diffs.All(d => d >= 1 && d <= 3);
+        bool isDecreasing = diffs.All(d => d >= -3 && d <= -1);
+
+        return isIncreasing || isDecreasing;
     }
 }
