1e2783ff4b306e17fced5cf1bcfd8afb69c391fd
Sune Frederiksen <sune.frederiksen@combitech.com>
2024-12-20 21:17:27 +0100

    Add day 3 solution for dotnet

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Day03;
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
 
 internal class Program
 {
+    private enum MatchType
+    {
+        Do,
+        Dont,
+        Mul
+    }
+
     static void Main()
     {
         /**
@@ -9,19 +18,90 @@ internal class Program
          * 
          */
 
-        var input = File.ReadAllText("Data/sample.txt");
+        var input = File.ReadAllText("Data/sample2.txt");
 
         Console.WriteLine(Part1(input));
         Console.WriteLine(Part2(input));
     }
 
-    static string Part1(string input)
+    public static int Part1(string input)
     {
-        return input;
+        var matches = Regex.Matches(input, @"mul\((\d+),(\d+)\)");
+
+        var pairs = matches
+            .Select(match =>
+            {
+                var a = int.Parse(match.Groups[1].Value);
+                var b = int.Parse(match.Groups[2].Value);
+                return (a, b);
+            })
+            .ToArray();
+
+        var result = pairs.Select(x => x.Item1 * x.Item2).Sum();
+        return result;
     }
 
-    static string Part2(string input)
+    public static int Part2(string input)
     {
-        return input;
+        var result = 0;
+        var index = 0;
+        var mulActive = true;
+
+        while (true)
+        {
+            var (match, matchType) = GetFirstMatch(input[index..]);
+            if (match == null)
+            {
+                break;
+            }
+
+            switch (matchType)
+            {
+                case MatchType.Do:
+                {
+                    mulActive = true;
+                    index += match.Index + match.Length;
+                    break;
+                }
+                case MatchType.Dont:
+                {
+                    mulActive = false;
+                    index += match.Index + match.Length;
+                    break;
+                }
+                case MatchType.Mul:
+                {
+                    if (mulActive)
+                    {
+                        var a = int.Parse(match.Groups[1].Value);
+                        var b = int.Parse(match.Groups[2].Value);
+                        result += a * b;
+                    }
+                    index += match.Index + match.Length;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static (Match, MatchType) GetFirstMatch(string input)
+    {
+        var doMatch = Regex.Match(input, @"do\(\)");
+        var dontMatch = Regex.Match(input, @"don't\(\)");
+        var mulMatch = Regex.Match(input, @"mul\((\d+),(\d+)\)");
+
+        var matches = new[]
+        {
+            (doMatch, MatchType.Do),
+            (dontMatch, MatchType.Dont),
+            (mulMatch, MatchType.Mul)
+        };
+
+        return matches
+            .Where(m => m.Item1.Success)
+            .OrderBy(m => m.Item1.Index)
+            .FirstOrDefault();
     }
 }
